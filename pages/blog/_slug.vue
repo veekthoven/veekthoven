62f779f144e3d6afa84172f47e2a15a33bbf6f8e
Victor Abbah Nkoms <veekthoven@gmail.com>
2020-07-08 23:30:47 +0100

    added alt text to featured images

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -17,7 +17,7 @@
 		</div>
 
 		<div class="mb-3 mt-5">
-			<img :src="article.image.url" class="w-full rounded-lg shadow">
+			<img :src="article.image.url" class="w-full rounded-lg shadow" :alt="article.title">
 			<p class="text-xs text-gray-600 mt-1" v-if="article.image.caption">{{ article.image.caption }}</p>
 		</div>
 
@@ -25,17 +25,6 @@
 			<nuxt-content :document="article" />
 		</div>
 		
-		<!-- <div class="flex justify-between">
-			<nuxt-link v-if="prev" :to="{ name: 'blog-slug', params: { slug: prev.slug } }" class="text-xs font-medium inline-flex items-center">
-				<svg class="h-4 w-4 mr-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
-				{{ prev.title }}
-			</nuxt-link>
-
-			<nuxt-link v-if="next" :to="{ name: 'blog-slug', params: { slug: next.slug } }" class="text-xs font-medium inline-flex items-center">
-				{{ next.title }}
-				<svg class="h-4 w-4 ml-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-			</nuxt-link>
-		</div> -->
 		<hr class="text-gray-600 mt-5 mb-3">
 		
 		<p class="text-xl font-bold">Subscribe to my newsletter</p>
